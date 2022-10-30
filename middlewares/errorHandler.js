25be48cd7f72fa77cd64f22d4a9adb30e822e298
muhammad-jiku <muhammadjiku364@gmail.com>
2022-10-30 20:12:36 +0600

    error handling setup build

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,4 +1,5 @@
 // dependecies
+// require('dotenv').config()
 const createError = require('http-errors');
 
 // 404 error handler
@@ -9,9 +10,20 @@ const notFoundHandler = (req, res, next) => {
 // default error handler
 const errorHandler = (err, req, res, next) => {
   // sending data as variable
-  res.locals.title = 'Error Page';
-  res.locals.message = 'There is something went wrong!';
-  res.render('error');
+  res.locals.error =
+    process.env.NODE_ENV === 'development' ? err : { message: err.message };
+
+  res.status(err.status || 500);
+
+  if (res.locals.html) {
+    // html response
+    res.render('error', {
+      title: 'Error Page',
+    });
+  } else {
+    // json response
+    res.json(res.locals.error);
+  }
 };
 
 module.exports = {
