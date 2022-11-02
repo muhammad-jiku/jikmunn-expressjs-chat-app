9a02bf29b3d7413e09f6bc04162a97c4e178c2dc
muhammad-jiku <muhammadjiku364@gmail.com>
2022-11-02 14:10:39 +0600

    route added for avatar upload

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,11 +4,15 @@ const express = require('express');
 // importing files
 const { getUsers } = require('../controllers/userController');
 const decorateHtmlResHandler = require('../middlewares/common/decorateHtmlResHandler');
+const avatarUpload = require('../middlewares/users/avatarUpload');
 
 // defining router
 const router = express.Router();
 
 // sign in page
 router.get('/', decorateHtmlResHandler('Users'), getUsers);
+
+// add user's avatar
+router.post('/', avatarUpload);
 
 module.exports = router;
