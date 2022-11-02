9ca7fe2db796c6c61cebd704b0ada8f6afa240bd
muhammad-jiku <muhammadjiku364@gmail.com>
2022-11-02 18:17:11 +0600

    external file for user validation is added

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,11 +1,11 @@
 // dependencies
 const express = require('express');
-const { check } = require('express-validator');
 
 // importing files
 const { getUsers } = require('../controllers/userController');
 const decorateHtmlResHandler = require('../middlewares/common/decorateHtmlResHandler');
 const avatarUpload = require('../middlewares/users/avatarUpload');
+const { addUserValidators } = require('../middlewares/users/userValidator');
 
 // defining router
 const router = express.Router();
@@ -14,9 +14,6 @@ const router = express.Router();
 router.get('/', decorateHtmlResHandler('Users'), getUsers);
 
 // add user's avatar
-router.post('/', avatarUpload, [
-  check('name').isLength({ min: 1 }).withMessage('Name is required!!'),
-  check('email').isEmail().withMessage('Invalid email!!'),
-]);
+router.post('/', avatarUpload, addUserValidators);
 
 module.exports = router;
