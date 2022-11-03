fe3b24b11ff3ec25663c0591ee0a4d96c98addbe
muhammad-jiku <muhammadjiku364@gmail.com>
2022-11-03 19:21:29 +0600

    message and conversation schema created

---
+++ schemas/conversationSchema.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const conversationSchema = mongoose.Schema(
+  {
+    creator: {
+      id: mongoose.Types.ObjectId,
+      name: String,
+      avatar: String,
+    },
+
+    participant: {
+      id: mongoose.Types.ObjectId,
+      name: String,
+      avatar: String,
+    },
+    last_updated: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = conversationSchema;
