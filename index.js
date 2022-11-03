2dbb0d76ac349520cc6c5a21790eb20c92c59e72
muhammad-jiku <muhammadjiku364@gmail.com>
2022-11-03 20:30:10 +0600

    depndency setup for moment, socket.io and http is added

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // dependencies
 const express = require('express');
 const mongoose = require('mongoose');
+const http = require('http');
 const path = require('path');
 const dotenv = require('dotenv');
+const moment = require('moment');
 const cookieParser = require('cookie-parser');
 
 // import files
@@ -16,12 +18,20 @@ const inboxRouter = require('./routes/inboxRouter');
 
 // app initialization
 const app = express();
+const server = http.createServer(app);
 const port = process.env.PORT || 4000;
 
 // middleware
 dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// socket creation
+const io = require('socket.io')(server);
+global.io = io;
+
+// set comment as app locals
+app.locals.moment = moment;
 
 // database connection with mongoose
 const uri = `mongodb+srv://${process.env.DB_AUTHOR}:${process.env.DB_PASSWORD}@cluster0.pzzsrxw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
@@ -62,6 +72,6 @@ app.use(notFoundHandler);
 app.use(errorHandler);
 
 // listening to the port
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
