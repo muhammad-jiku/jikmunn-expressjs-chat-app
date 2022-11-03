cba368867e8ef5435e155408480bbe61fa77adf4
muhammad-jiku <muhammadjiku364@gmail.com>
2022-11-03 15:52:29 +0600

    middlefor checking cookies existance is added

--- middlewares/common/checkSignIn.js
+++ middlewares/common/checkSignIn.js
@@ -40,6 +40,18 @@ const checkSignIn = (req, res, next) => {
   }
 };
 
+const redirectSignedIn = function (req, res, next) {
+  let cookies =
+    Object.keys(req.signedCookies).length > 0 ? req.signedCookies : null;
+
+  if (!cookies) {
+    next();
+  } else {
+    res.redirect('/inbox');
+  }
+};
+
 module.exports = {
   checkSignIn,
+  redirectSignedIn,
 };
--- routes/signInRouter.js
+++ routes/signInRouter.js
@@ -7,6 +7,7 @@ const {
   signIn,
   signOut,
 } = require('../controllers/signInController');
+const { redirectSignedIn } = require('../middlewares/common/checkSignIn');
 const decorateHtmlResHandler = require('../middlewares/common/decorateHtmlResHandler');
 const {
   doLoginValidators,
@@ -20,7 +21,12 @@ const router = express.Router();
 const page_title = 'Sign In';
 
 // displaying sign-in page
-router.get('/', decorateHtmlResHandler(page_title), getSignIn);
+router.get(
+  '/',
+  decorateHtmlResHandler(page_title),
+  redirectSignedIn,
+  getSignIn
+);
 
 // sign-in
 router.post(
